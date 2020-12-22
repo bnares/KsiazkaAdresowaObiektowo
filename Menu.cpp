d54d14e5ff56e3e23dc5596f599e68f197dbcdcb
bnares <74542449+bnares@users.noreply.github.com>
2020-12-22 21:23:00 +0100

    Add files via upload

---
+++ Menu.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "Menu.h"
+
+
+using namespace std;
+
+char Menu:: wybierzOpcjeZMenuGlownego()
+{
+    char wybor;
+    MetodyPomocznicze pomoc;
+
+    system("cls");
+    cout << "    >>> MENU  GLOWNE <<<" << endl;
+    cout << "---------------------------" << endl;
+    cout << "1. Rejestracja" << endl;
+    cout << "2. Logowanie" << endl;
+    cout << "9. Koniec programu" << endl;
+    cout << "---------------------------" << endl;
+    cout << "Twoj wybor: ";
+    wybor = pomoc.wczytajZnak();
+
+    return wybor;
+}
+
+
+char Menu:: wybierzOpcjeZMenuUzytkownika()
+{
+    char wybor;
+    MetodyPomocznicze pomoc;
+
+
+    system("cls");
+    cout << " >>> MENU UZYTKOWNIKA <<<" << endl;
+    cout << "---------------------------" << endl;
+    cout << "1. Dodaj adresata" << endl;
+    cout << "2. Wyszukaj po imieniu" << endl;
+    cout << "3. Wyszukaj po nazwisku" << endl;
+    cout << "4. Wyswietl adresatow" << endl;
+    cout << "5. Usun adresata" << endl;
+    cout << "6. Edytuj adresata" << endl;
+    cout << "---------------------------" << endl;
+    cout << "7. Zmien haslo" << endl;
+    cout << "8. Wyloguj sie" << endl;
+    cout << "---------------------------" << endl;
+    cout << "Twoj wybor: ";
+    wybor = pomoc.wczytajZnak();
+
+    return wybor;
+
+}
---
+++ Menu.h
@@ -0,0 +1,16 @@
+#ifndef  MENU_H
+#include "MetodyPomocnicze.h"
+#include "PlikiZUzytkownikami.h"
+#define MENU_H
+
+class Menu
+{
+public:
+
+    char wybierzOpcjeZMenuGlownego();
+    char wybierzOpcjeZMenuUzytkownika();
+};
+
+
+
+#endif
